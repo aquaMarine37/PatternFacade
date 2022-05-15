3772d61cb5d6bed75732539d138ad14566bdf0e5
Marina Dudko <aquamarine23@mail.ru>
2022-05-15 21:20:30 +0300

    add classes

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bin = new BinOps();
+        System.out.println(bin.mult("111", "11"));
+        System.out.println(bin.sum("001", "101"));
+    }
+}
